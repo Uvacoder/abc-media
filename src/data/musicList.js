6f959e15b5ee810d77126743ce989dff0eb3a8e4
SJellen <sjellen@me.com>
2021-11-22 12:15:26 -0500

    start to fill music filess

--- src/data/musicList.js
+++ src/data/musicList.js
@@ -6,6 +6,24 @@ const musicListened = [
 
 ]
 
-const handshakeFiveMusic = []
+const handshakeFiveMusic = [
+    {   
+        id: "8e4600bc-cd9a-4822-b615-d9095dc6cf8e",
+        cover: "https://ia801309.us.archive.org/10/items/mbid-57fc464a-a8da-3b4c-a7de-6ca260297f65/mbid-57fc464a-a8da-3b4c-a7de-6ca260297f65-11436631253.png",
+        artist: "Led Zeppelin",
+        title: "Led Zeppelin II",
+        rating: 5,
+        review: ""
+    },
+    {   
+        id: "9162580e-5df4-32de-80cc-f45a8d8a9b1d",
+        cover: "https://ia600306.us.archive.org/26/items/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c-18073305778.jpg",
+        artist: "The Beatles",
+        title: "Abbey Road",
+        rating: 5,
+        review: ""
+    },
+
+]
 
 export {musicListening, musicListened, handshakeFiveMusic}
