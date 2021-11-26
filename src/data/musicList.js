e17edb8e7db28e157bbf1310e096f06b815dba21
SJellen <sjellen@me.com>
2021-11-26 10:40:26 -0500

    edits on the musiclist

--- src/data/musicList.js
+++ src/data/musicList.js
@@ -65,7 +65,7 @@ const musicListened = [
 const handshakeFiveMusic = [
     {   
         id: "8e4600bc-cd9a-4822-b615-d9095dc6cf8e",
-        cover: "https://ia801309.us.archive.org/10/items/mbid-57fc464a-a8da-3b4c-a7de-6ca260297f65/mbid-57fc464a-a8da-3b4c-a7de-6ca260297f65-11436631253.png",
+        cover: "https://ia801309.us.archive.org/10/items/mbid-57fc464a-a8da-3b4c-a7de-6ca260297f65/mbid-57fc464a-a8da-3b4c-a7de-6ca260297f65-11436631253_thumb250.jpg",
         artist: "Led Zeppelin",
         title: "Led Zeppelin II",
         rating: 5,
@@ -73,7 +73,7 @@ const handshakeFiveMusic = [
     },
     {   
         id: "9162580e-5df4-32de-80cc-f45a8d8a9b1d",
-        cover: "https://ia600306.us.archive.org/26/items/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c-18073305778.jpg",
+        cover: "https://ia600306.us.archive.org/26/items/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c-18073305778_thumb250.jpg",
         artist: "The Beatles",
         title: "Abbey Road",
         rating: 5,
@@ -81,7 +81,7 @@ const handshakeFiveMusic = [
     },
     {   
         id: "bbb10475-1e88-4dc6-8ed4-4535103add22",
-        cover: "https://ia600306.us.archive.org/26/items/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c-18073305778.jpghttps://ia600207.us.archive.org/24/items/mbid-bbb10475-1e88-4dc6-8ed4-4535103add22/mbid-bbb10475-1e88-4dc6-8ed4-4535103add22-6531720275.jpg",
+        cover: "https://ia800207.us.archive.org/24/items/mbid-bbb10475-1e88-4dc6-8ed4-4535103add22/mbid-bbb10475-1e88-4dc6-8ed4-4535103add22-6531720275_thumb250.jpg",
         artist: "Otis Redding",
         title: "The King of Soul",
         rating: 5,
@@ -89,7 +89,7 @@ const handshakeFiveMusic = [
     },
     {   
         id: "f739297d-4c8d-401c-843f-6afb86140f58",
-        cover: "https://ia600306.us.archive.org/26/items/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c/mbid-fb70321f-78df-30ff-92de-21a4bc9ca16c-18073305778.jpghttps://ia600207.us.archive.org/24/items/mbid-bbb10475-1e88-4dc6-8ed4-4535103add22/mbid-bbb10475-1e88-4dc6-8ed4-4535103add22-6531720275.jpg",
+        cover: "https://ia800101.us.archive.org/15/items/mbid-f739297d-4c8d-401c-843f-6afb86140f58/mbid-f739297d-4c8d-401c-843f-6afb86140f58-18379205951_thumb250.jpg",
         artist: "Curtis Mayfield",
         title: "Superfly",
         rating: 5,
@@ -97,7 +97,7 @@ const handshakeFiveMusic = [
     },
     {   
         id: "e1262988-757c-4861-9778-65e7d4ef5c55",
-        cover: "https://ia600502.us.archive.org/8/items/mbid-e1262988-757c-4861-9778-65e7d4ef5c55/mbid-e1262988-757c-4861-9778-65e7d4ef5c55-8016931920.jpg",
+        cover: "https://ia600502.us.archive.org/8/items/mbid-e1262988-757c-4861-9778-65e7d4ef5c55/mbid-e1262988-757c-4861-9778-65e7d4ef5c55-8016931920_thumb250.jpg",
         artist: "Steve Winwood",
         title: "Revolutions: The Very Best of Steve Winwood",
         rating: 5,
