5c7c7a7ab75cf7e89c42c95d54bbe99f47f754f8
SJellen <sjellen@me.com>
2021-11-27 13:31:27 -0500

    starting of creating a games section

---
+++ src/components/Games.js
@@ -0,0 +1,16 @@
+import React, {useState, useContext} from 'react'
+import {gamesPlaying, gamesPlayed, handshakeFiveGames} from '../data/gameList'
+import { Context } from '../Context'
+
+function Games() {
+
+    const [handshakeReview, setHandshakeReview] = useState()
+    const [watchedReview, setWatchedReview] = useState()
+    const [watchingReview, setWatchingReview] = useState()
+
+    return (
+        <div></div>
+    )
+}
+
+export default Games
