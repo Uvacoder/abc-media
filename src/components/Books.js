f99c97e3f68799cb6a6bd77e2cc6a3db7704ad45
SJellen <sjellen@me.com>
2021-12-01 09:18:44 -0500

    variable name changes

--- src/components/Books.js
+++ src/components/Books.js
@@ -7,11 +7,9 @@ function Books() {
     const {fullStarMaker, halfStarMaker, reviewToggle, filterSet, dimmer, handleBooksReadArrowToggle, booksReadingSlice, booksReadSlice} = useContext(Context)
       
     const [handshakeReview, setHandshakeReview] = useState()
-    const [watchedReview, setWatchedReview] = useState()
-    const [watchingReview, setWatchingReview] = useState()
+    const [readReview, setReadReview] = useState()
+    const [readingReview, setReadingReview] = useState()
     
-   
-
    const bookHandshake = handshakeFiveBooks.map((book, index) => (
         <div className="bookHandshake"  key={book.id} style={filterSet(book, handshakeReview)}>
             <img src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`} alt="book covers" className="book" />
@@ -35,19 +33,19 @@ function Books() {
     ))
 
     const nowReading = booksReading.slice(0, booksReadingSlice).map((book, index) => (
-        <div className="now_reading"    key={book.id} style={filterSet(book, watchingReview)}>
+        <div className="now_reading"    key={book.id} style={filterSet(book, readingReview)}>
             <img src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`} alt="book covers" className="book" />
             <div  className="word_box">
             <h4 className="title">{book.title}</h4>
             <span className="star-container" ><h4 className="star">{ fullStarMaker(book.rating)}</h4><h4 className="half-star">{halfStarMaker(book.rating)}</h4></span>
-             { watchingReview === book.id ? 
+             { readingReview === book.id ? 
                 <div>
-                <i className="material-icons arrow_drop_up_icon" onClick={() => reviewToggle(index, booksReading, watchingReview, setWatchingReview)} 
+                <i className="material-icons arrow_drop_up_icon" onClick={() => reviewToggle(index, booksReading, readingReview, setReadingReview)} 
                 style={{display: book.review === "" ? "none" : ""}}>arrow_drop_up_icon</i>
                 <p className="review" style={{display:  book.review ? "block" : "none"}}>{book.review}</p>
                 </div>
                  :
-                 <i className="material-icons arrow_right_icon" onClick={() => reviewToggle(index, booksReading, watchingReview, setWatchingReview)}
+                 <i className="material-icons arrow_right_icon" onClick={() => reviewToggle(index, booksReading, readingReview, setReadingReview)}
                  style={{display: book.review === "" ? "none" : ""}}
                  >arrow_right_icon</i>
             }
@@ -56,19 +54,19 @@ function Books() {
     ))
 
     const recentlyRead = booksRead.slice(0, booksReadSlice).map((book, index) => (
-        <div className="recently_read"  key={book.id} style={filterSet(book, watchedReview)} >
+        <div className="recently_read"  key={book.id} style={filterSet(book, readReview)} >
             <img src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`} alt="book covers" className="book" />
             <div  className="word_box">
             <h4 className="title">{book.title}</h4>
             <span className="star-container" ><h4 className="star">{ fullStarMaker(book.rating)}</h4><h4 className="half-star">{halfStarMaker(book.rating)}</h4></span>
-            { watchedReview === book.id ? 
+            { readReview === book.id ? 
                 <div>
-                <i className="material-icons arrow_drop_up_icon" onClick={() => reviewToggle(index, booksRead, watchedReview, setWatchedReview)} 
+                <i className="material-icons arrow_drop_up_icon" onClick={() => reviewToggle(index, booksRead, readReview, setReadReview)} 
                 style={{display: book.review === "" ? "none" : ""}}>arrow_drop_up_icon</i>
                 <p className="review" style={{display:  book.review ? "block" : "none"}}>{book.review}</p>
                 </div>
                  :
-                 <i className="material-icons arrow_right_icon" onClick={() => reviewToggle(index, booksRead, watchedReview, setWatchedReview)}
+                 <i className="material-icons arrow_right_icon" onClick={() => reviewToggle(index, booksRead, readReview, setReadReview)}
                  style={{display: book.review === "" ? "none" : ""}}
                  >arrow_right_icon</i>
             }
@@ -86,23 +84,18 @@ function Books() {
             <h2 className="sub_section_title" title="currentlyReading" style={dimmer}>Currently Reading</h2>
             <div className="currently_reading_flex">
                 {nowReading}
-                {/* <div className="slice_toggle" >
-                    <span className="toggle_arrow" ><ArrowDropDownCircleIcon style={{ fontSize: 50 }} onClick={handleBooksReadingArrowToggle} className={booksReadingSlice === 10 ? '' : 'rotate_arrow'} /></span>
-                </div> */}
             </div>
             <h2 className="sub_section_title"  title="recentlyRead" style={dimmer}>Recently Read</h2>
             <div className="recently_read_flex">
                 {recentlyRead}
                 <div className="slice_toggle" >
                     <span className="toggle_arrow" >
-                    {/* <ArrowDropDownCircleIcon style={{ fontSize: 50 }} onClick={handleBooksReadArrowToggle} className={booksReadSlice === 10 ? '' : 'rotate_arrow'} /> */}
                     <svg 
                         xmlns="http://www.w3.org/2000/svg" 
                         width="24" height="24" 
                         viewBox="0 0 24 24" 
                         fill="white"
-                        style={{ fontSize: 50 }} onClick={handleBooksReadArrowToggle} className={booksReadSlice === 10 ? '' : 'rotate_arrow'}
-                        
+                        style={{ fontSize: 50 }} onClick={handleBooksReadArrowToggle} className={booksReadSlice === 10 ? '' : 'rotate_arrow'}                        
                         >
                         <path d="M12 21l-12-18h24z" />
                     </svg>
