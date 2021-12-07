bda0c21e363476e66abb55452dc9a8dd30a4cec9
SJellen <sjellen@me.com>
2021-12-07 12:56:23 -0500

    header seperate stylesheet created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
 import { Context } from '../Context'
+import '../style/Header.css'
 
 function Header(){
     const {fullDim, handleHamburgerClick, dropDownNavClick, isNavOpen} = useContext(Context)
