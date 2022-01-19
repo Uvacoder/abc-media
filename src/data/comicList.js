f2fe0c941b0be2037abe39bf3d53b192feee6f6c
SJellen <sjellen@me.com>
2022-01-19 15:28:06 -0500

    work to start a comics section

---
+++ src/data/comicList.js
@@ -0,0 +1,58 @@
+const comicsReading = [
+    {   id: "",
+        title: "",
+        isbn: "",
+        rating: 3.5,
+        review: ""
+    },
+  
+    
+
+].reverse()
+
+const comicsRead = [
+    {   id: "",
+        title: "",
+        isbn: "",
+        rating: 3.5,
+        review: ""
+    },
+    
+
+]
+
+const handshakeFiveComics = [
+    {   id: "9780785123262",
+        title: "New X-Men",
+        isbn: "9780785123262",
+        rating: 4.5,
+        review: ""
+    },
+    {   id: "9780613536707",
+        title: "The Dark Knight Returns",
+        isbn: "9780613536707",
+        rating: 5,
+        review: ""
+    },
+    {   id: "",
+        title: "",
+        isbn: "",
+        rating: 3.5,
+        review: ""
+    },
+    {   id: "",
+        title: "",
+        isbn: "",
+        rating: 3.5,
+        review: ""
+    },
+    {   id: "",
+        title: "",
+        isbn: "",
+        rating: 3.5,
+        review: ""
+    },
+
+]
+
+export {comicsReading, comicsRead, handshakeFiveComics}
