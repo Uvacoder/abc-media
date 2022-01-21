b107024d56ba4525a3672ab2319359a34272f77b
SJellen <sjellen@me.com>
2022-01-21 12:02:11 -0500

    added a review

--- src/data/comicList.js
+++ src/data/comicList.js
@@ -27,7 +27,7 @@ const handshakeFiveComics = [
         title: "New X-Men",
         isbn: "9780785123262",
         rating: 4.5,
-        review: ""
+        review: "In an industry where most of the writers have one idea and protect it like a winning lottery ticket, Grant Morrison never runs out of ideas. You've seen some of his ideas whether you knew it or not. The one good Matrix movie is a blatant rip off of his series The Invisibles. He had a relatively short but productive stint at Marvel in the early 2000s and New X-men was the best of it. New X-Men is at it's best when Frank Quitely is drawing the pages, but thats also close to the start of the run and helps separate it from the rest of the series. The run closes with a more photo realistic Silvestri but thats more of an epilog to the story. There are some interesting ideas that still linger to this day and I also enjoyed Emma Frost in the X-Men. Its hard to explain just how bad off Marvel was before Joe Quesada became Editor-in-Chief. He launched a handful of new imprints and breathed new live into stale characters with his leadership. The company went from almost bankrupt, to having a renaissances and making movies, to being bought by Disney. The state of comics as they are right now(near death), the early 2000s was the last great age of comic books."
     },
     {   id: "9780613536707",
         title: "The Dark Knight Returns",
